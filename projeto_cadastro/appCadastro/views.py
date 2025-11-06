1550193cc9998656a8341905c55091a40b221951
Nicholas <nychihara@gmail.com>
2025-11-06 17:52:44 -0300

    tlod

--- projeto_cadastro/appCadastro/views.py
+++ projeto_cadastro/appCadastro/views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Usuario
+
 
 def home(request):
     return render(request, 'usuarios/home.html')
 
 def usuario(request):
-    novoUsuario = Usuario()
-    novoUsuario.nome = request.POST.get('nome')
-    novoUsuario.idade = request.POST.get('idade')
-    novoUsuario.save()
+    if request.method == 'POST':
+        novoUsuario = Usuario()
+        novoUsuario.nome = request.POST.get('nome')
+        novoUsuario.idade = request.POST.get('idade')
+        novoUsuario.save()
+        return redirect('listagemUsuario')
 
     usuarios = {
         'usuarios': Usuario.objects.all()
     }
 
     return render(request, 'usuarios/usuarios.html', usuarios)
+
+def deletarUsuario(request, idUsuario):
+    usuario = get_object_or_404(Usuario, idUsuario=idUsuario)
+    usuario.delete()
+    return redirect('listagemUsuario')
--- projeto_cadastro/projeto_cadastro/urls.py
+++ projeto_cadastro/projeto_cadastro/urls.py
@@ -3,5 +3,6 @@ from appCadastro import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('usuario/',views.usuario,name='listagemUsuario')
+    path('usuario/',views.usuario,name='listagemUsuario'),
+    path('deletarUsuario/<int:idUsuario>/', views.deletarUsuario, name='deletarUsuario'),
 ]
